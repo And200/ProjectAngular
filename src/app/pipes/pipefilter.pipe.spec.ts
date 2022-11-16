b057528a970c14b5bb5ee2703b50f3909242e7d1
unknown <nosredna2002@gmail.com>
2022-11-16 14:26:07 -0500

    Revert "project Angular Related Tables Product And Category"
    This reverts commit d9f7d44a18fd5e1b524459350604d73ada0e59cc.

--- src/app/pipes/pipefilter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PipefilterPipe } from './pipefilter.pipe';
-
-describe('PipefilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PipefilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
